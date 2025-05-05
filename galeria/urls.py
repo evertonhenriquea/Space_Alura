5634a6cefeac12423e03de0245bf4a13bc014cab
Everton Henrique Almeida Rezende <evertonsonhador15243@gmail.com>
2025-05-04 21:02:15 -0300

    Imagens

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import *
 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('imagem', imagem, name='imagem'),
 ]
