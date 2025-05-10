02f8ab064969b16a6043718e8c508d7db1e5a7d2
Everton Henrique Almeida Rezende <evertonsonhador15243@gmail.com>
2025-05-09 23:35:04 -0300

    Descrição das Fotos

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1: {"nome": "Nebulosa De Carina",
+        "legenda": " A nebulosa se encontra a uma distância estimada entre 6.500 e 10.000 anos-luz da Terra. Ele aparece na direção da constelação de Carina e situa-se no braço de Carina-Sagitário."},
+    2: {"nome": "Earthrise",
+        "legenda": "A Terra, ao fundo, é visível como um globo azul e branco, com nuvens e massas de terra claramente definidas."}
+}
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
