8d12e0ea394e3bd438394c27239d568972a79a10
Everton Henrique Almeida Rezende <evertonsonhador15243@gmail.com>
2025-06-17 22:59:32 -0300

    Partial de alertas

--- setup/settings.py
+++ setup/settings.py
@@ -135,3 +135,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages
+from django.contrib.messages import constants as messages
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
